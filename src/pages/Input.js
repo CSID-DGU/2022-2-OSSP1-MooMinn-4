5b1c1e4e9c9c3ca1d91a81f8146d0c20ff13af32
wkkyu088 <dnjsrbwls08@gmail.com>
2022-05-20 04:18:43 +0900

    add input file button

--- src/pages/Input.js
+++ src/pages/Input.js
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import FileUploadRoundedIcon from '@mui/icons-material/FileUploadRounded';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import './css/Input.css';
 
 const Input = () => {
+    const input = React.useRef(null);
+    const [placeholder, setPlaceholder] = React.useState('');
+
     return (
         <div className="fade-in">
             <Stack direction="row" justifyContent="space-between" className="nav">
@@ -22,8 +28,29 @@ const Input = () => {
                 </Box>
             </Stack>
             <Box className="sub_title">
-            파일업로드
+                파일업로드
             </Box>
+            <Box className="guide">
+                가이드 사진
+            </Box>
+            <Stack className="input_area" flexDirection={"row"}>
+                <Stack 
+                    className="upload_btn"
+                    flexDirection={"row"}
+                    onClick={() => {input.current?.click();}}
+                >
+                    <span className="upload_icon"><FileUploadOutlinedIcon/></span>
+                    <span className="upload">파일 업로드</span>
+                </Stack>
+                <Box className="file_name">{placeholder}</Box>
+                <input
+                    type={"file"}
+                    accept={".xlsx,.xls"}
+                    ref={input}
+                    style={{display:'none'}}
+                    onChange={(e) => {e.target.files && setPlaceholder(e.target.files[0].name);}}
+                />
+            </Stack>
             <Box className="btn_area">
                 <Link to='/result'>
                     <button className="btn">계산</button>
